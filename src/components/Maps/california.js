ad3bace55fc6d5f0f70aec8b904f5559981b7cda
蔣曜安 <yeoan11317@gmail.com>
2018-12-11 10:51:22 +0800

    add more charts

--- src/components/Maps/california.js
+++ src/components/Maps/california.js
@@ -8,6 +8,9 @@ class CaliforniaMap extends React.Component {
     var thisClass = this;
     var counter = 3;
     var clouds = [];
+    var temps = [];
+    var wind = [];
+    var rain = [];
     axios.get('../../../california.json')
   .then(function (response) {
     response.data.list.map((item)=>{
@@ -15,9 +18,24 @@ class CaliforniaMap extends React.Component {
         "label": counter.toString(),
         "value": item.clouds.all
       })
+      temps.push({
+        "label": counter.toString(),
+        "value": item.main.temp
+      })
+      wind.push({
+        "label": counter.toString(),
+        "value": item.wind.speed
+      })
+      rain.push({
+        "label": counter.toString(),
+        "value": item.rain["3h"]===undefined ? 0 : item.rain["3h"]
+      })
       counter = parseInt(counter + 3);
     });
     thisClass.renderCloudsCharts(clouds);
+    thisClass.renderTempsCharts(temps);
+    thisClass.renderWindCharts(wind);
+    thisClass.renderRainCharts(rain)
   })
   .catch(function (error) {
     // handle error
@@ -38,15 +56,15 @@ class CaliforniaMap extends React.Component {
     FusionCharts.ready(function() {
   var avgBallChart = new FusionCharts({
     type: 'line',
-    renderAt: 'chart-container',
-    width: '50%',
+    renderAt: 'chart-container-clouds',
+    width: '100%',
     height: '400',
     dataFormat: 'json',
     dataSource: {
       "chart": {
-        "caption": "Clouds",
+        "caption": "Cloud",
         "subCaption": "5 days per 3hrs",
-        "numberSuffix": "",
+        "numberSuffix": "%",
         "showValues": "0",
         "rotateLabels": "1",
         "slantLabels": "1",
@@ -66,9 +84,108 @@ class CaliforniaMap extends React.Component {
 });
   }
 
+  renderTempsCharts(temps){
+    FusionCharts.ready(function() {
+  var avgBallChart = new FusionCharts({
+    type: 'line',
+    renderAt: 'chart-container-temps',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Temperature",
+        "subCaption": "5 days per 3hrs",
+        "numberSuffix": "F",
+        "showValues": "0",
+        "rotateLabels": "1",
+        "slantLabels": "1",
+        "showHoverEffect": "1",
+        "canvasPadding": "10",
+        "yaxisminvalue": "89",
+        "showaxislines": "1",
+        "anchorRadius": "4",
+        "anchorBgColor": "#876EA1",
+        "anchorHoverRadius": "8",
+        "theme": "hulk-light",
+
+      },
+      "data": temps
+    }
+  }).render();
+  });
+  }
+
+  renderWindCharts(windSpeed){
+    FusionCharts.ready(function() {
+  var avgBallChart = new FusionCharts({
+    type: 'line',
+    renderAt: 'chart-container-wind',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Wind Speed",
+        "subCaption": "5 days per 3hrs",
+        "numberSuffix": "meter/sec",
+        "showValues": "0",
+        "rotateLabels": "1",
+        "slantLabels": "1",
+        "showHoverEffect": "1",
+        "canvasPadding": "10",
+        "yaxisminvalue": "89",
+        "showaxislines": "1",
+        "anchorRadius": "4",
+        "anchorBgColor": "#876EA1",
+        "anchorHoverRadius": "8",
+        "theme": "hulk-light",
+
+      },
+      "data": windSpeed
+    }
+  }).render();
+  });
+  }
+
+  renderRainCharts(rain){
+    FusionCharts.ready(function() {
+  var avgBallChart = new FusionCharts({
+    type: 'line',
+    renderAt: 'chart-container-rain',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      "chart": {
+        "caption": "Rain",
+        "subCaption": "5 days per 3hrs",
+        "numberSuffix": "mm",
+        "showValues": "0",
+        "rotateLabels": "1",
+        "slantLabels": "1",
+        "showHoverEffect": "1",
+        "canvasPadding": "10",
+        "yaxisminvalue": "89",
+        "showaxislines": "1",
+        "anchorRadius": "4",
+        "anchorBgColor": "#876EA1",
+        "anchorHoverRadius": "8",
+        "theme": "hulk-light",
+
+      },
+      "data": rain
+    }
+  }).render();
+  });
+  }
+
   constructor(props) {
     super(props);
     this.renderCloudsCharts = this.renderCloudsCharts.bind(this);
+    this.renderTempsCharts = this.renderTempsCharts.bind(this);
+    this.renderWindCharts = this.renderWindCharts.bind(this);
+    this.renderRainCharts = this.renderRainCharts.bind(this);
   }
 
   render() {
@@ -77,7 +194,18 @@ class CaliforniaMap extends React.Component {
       <div class="col-lg-12">
           <h1 class="page-header">Weather</h1>
       </div>
-      <div id="chart-container"></div>
+      <div class="col-lg-6">
+      <div id="chart-container-clouds"></div>
+      </div>
+      <div class="col-lg-6">
+      <div id="chart-container-temps"></div>
+      </div>
+      <div class="col-lg-6">
+      <div id="chart-container-wind"></div>
+      </div>
+      <div class="col-lg-6">
+      <div id="chart-container-rain"></div>
+      </div>
       </div>
     );
   }
