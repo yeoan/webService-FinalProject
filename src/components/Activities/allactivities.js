18b84894c3aa546ce572078b7fb7bdff796c7a9c
蔣曜安 <yeoan11317@gmail.com>
2018-12-12 16:11:05 +0800

    add card view

--- src/components/Activities/allactivities.js
+++ src/components/Activities/allactivities.js
@@ -9,14 +9,29 @@ class Allactivities extends React.Component {
   }
 
   render() {
+    var activitiesList = ['amusement park','badminton','basketball','bicycle','biking','camping','couch potato','esport','fly a kite','get warm by stove','go to a hot spring','go to karaoke','gym','have a picnic','hiking','hunting','jogging','play board game','play snow day','programming','reading','roller skate','see a movie','shopping','sightseeing','skating','take a sunbath','visit a museum','volleyball'];
     return (
       <div id="page-wrapper">
           <div class="container-fluid">
               <div class="row">
-
-      <div class="col-lg-12">
-          <h1 class="page-header">All Activities</h1>
-          </div>
+                <div class="col-lg-12">
+                <h1 class="page-header">All Activities</h1>
+                </div>
+              {
+                activitiesList.map((item)=>{
+                  return (
+                    <div class="col-lg-4">
+                      <div class="card">
+                      <img src={'../src/images/'+item+'.jpg'} style={{width:'100%',height:'200px'}}/>
+                        <div class="cardcontainer">
+                          <h4><b>{item}</b></h4>
+                          <p>Architect & Engineer</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })
+              }
       </div>
       </div>
       </div>
